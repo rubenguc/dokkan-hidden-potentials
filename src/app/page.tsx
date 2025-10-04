8721ddaacc854fcd495a867d682d25d1354ebc58
Ruben <rubdeivis@gmail.com>
2025-10-04 11:37:34 -0400

    fix: fix import errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { Footer } from "@/features/home/components/footer";
-import { Header } from "@/features/home/components/header";
+import { Footer } from "@/features/home/components/home-footer";
+import { Header } from "@/features/home/components/home-header";
 import { useGetCharacters } from "@/features/characters/hooks/useGetCharacters";
 import type { SearchParams } from "@/interfaces";
 import { CharacterCard } from "@/features/characters/components/character-card";
-import { Filters } from "@/features/home/components/filters";
-import { Pagination } from "@/features/home/components/pagination";
+import { Filters } from "@/features/home/components/home-filters";
+import { Pagination } from "@/features/home/components/home-pagination";
 
 export default async function Home({
   searchParams,
